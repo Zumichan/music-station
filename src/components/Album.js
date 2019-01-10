af91f5ebb2a77ae01934377466a2c607d5b8ccf4
zumichan <azumi.y.callanan@gmail.com>
2019-01-10 11:15:07 -0800

    Implemented an audio playback functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,9 +9,46 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album //store const album as a property of state object
+      album: album, //store const album as a property of state object
+      currentSong: album.songs[0],
+      isPlaying: false,
+      isHovered: false
+    };
+    //audio element doesn't need to be attached to the DOM, that's why it's outside of the state
+    //audio element needs to be accessed within the class methods, therefore assigned to 'this'
+    this.audioElement = document.createElement('audio');
+    //playback will start on the first track
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    //change the playback song
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song){
+    const isSameSong = this.state.currentSong === song;
+    if(this.state.isPlaying && isSameSong){//If the user clicked on the currently playing song
+      this.pause();
+    } else {
+      if(!isSameSong){
+        this.setSong(song);
+      }
+      this.play();
     }
   }
+
   render() {
     return (
       <section className="album">
@@ -30,11 +67,31 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
-           {this.state.album.songs.map((album,index) =>
-            <tr>
-              <td>{index+1}</td>
-              <td>{album.title}</td>
-              <td>{album.duration}</td>
+           {
+             this.state.album.songs.map((song,index) =>
+             <tr className="song"
+               key={index}
+               onClick={() => this.handleSongClick(song)}
+               onMouseEnter={() => this.setState({ isHovered: index+1 })}
+               onMouseLeave={() => this.setState({ isHovered: false })}
+             >
+               <td className="song-actions">
+                 { //Current selected song is not playing?
+                   (this.state.currentSong === song && this.state.isPlaying)?
+                   //If the current song is playing: show a pause button
+                   //If the current song is not playing: show a play button
+                   <span className={this.state.isPlaying? "ion-md-pause" : "ion-md-play"}></span>
+                   :
+                   (this.state.isHovered === index+1)?
+                   //When the song is hovered, show a play icon
+                   <span className="ion-md-play"></span>
+                   :
+                   //when the song is not hovered, show a song number
+                   <span className="song-number">{index+1}</span>
+                 }
+               </td>
+               <td className="song-title">{song.title}</td>
+               <td className="song-duration">{song.duration}</td>
             </tr>
            )}
            </tbody>
