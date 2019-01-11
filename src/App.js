f19c5ccc1d748ed16203d4fb4d7f519dcb1c9e2c
zumichan <azumi.y.callanan@gmail.com>
2019-01-11 10:26:22 -0800

    Applied styling

--- src/App.js
+++ src/App.js
@@ -9,18 +9,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to ='/'>Landing</Link>
-            <Link to ='/library'>Library</Link>
-            <Link to ='/album'>Album</Link>
+        <header className="App-header">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container">
+              <Link className="navbar-brand" to ='/'>Music Station</Link>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="navbar-link" href="/library">Library</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </nav>
-          <h1>Music Station</h1>
         </header>
+
         <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          <Route path="/album/:slug" component={Album} />
+          <nav className="navbar-header">
+            <Route exact path="/" component={Landing} />
+            <Route path="/library" component={Library} />
+            <Route path="/album/:slug" component={Album} />
+          </nav>
         </main>
       </div>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,43 @@
 import React from 'react';
 
 const Landing = () => (
-  <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
+  <main role="main">
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-3 text-white">Turn the music up!</h1>
+        <p className="text-white">Access your music library from anywhere</p>
+        <p><a className="btn btn-secondary btn-lg" href="/library" role="button">Go to the library >></a></p>
+    </div>
+  </div>
 
-    <section className="selling-points">
-      <div className="point">
-        <h2 className="point-title">Choose your music</h2>
-        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4 text-white text-center">
+        <img className="music-note" src="music-note.png" alt="music note icon"></img>
+        <h2>Choose your music</h2>
+        <p>The world is full of music; why should you have to music that someone else chose?</p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
-      <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-        <p className="point-description">No arbitrary limits. No distractions.</p>
+      <div className="col-md-4 text-white text-center">
+        <img className="block-icon" src="cancel.png" alt="block icon"></img>
+        <h2>Unlimited, streaming, ad-free</h2>
+        <p>No arbitrary limits. No distractions.</p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
-      <div className="point">
-        <h2 className="point-title">Mobile enabled</h2>
-        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+      <div className="col-md-4 text-white text-center">
+        <img className="mobile-icon" src="iPhone.png" alt="mobile icon"></img>
+        <h2>Mobile enabled</h2>
+        <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+        <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
-    </section>
-  </section>
+    </div>
+  </div>
+
+  <footer className="container text-white">
+      <p>©︎Music Station 2019</p>
+  </footer>
+
+  </main>
 );
 
 export default Landing;
